5ebbe610bb236393715beb3683dad3c70fbdc19a
khmelinin <khmelinin2001@gmail.com>
2020-03-03 15:02:49 +0200

    finally

--- Labs(SRM)/Source.cpp
+++ Labs(SRM)/Source.cpp
@@ -16,17 +16,17 @@ void print(int n, int m, float** matrix)
     cout << endl;
 }
 
-bool ok(int n, int m, float** matrix)
-{
-    if (matrix[0][0] > matrix[0][1] + matrix[0][2] + matrix[0][3] + matrix[0][4] &&
-        matrix[1][1] > matrix[1][0] + matrix[1][2] + matrix[1][3] + matrix[1][4] &&
-        matrix[2][2] > matrix[2][1] + matrix[2][0] + matrix[2][3] + matrix[2][4] &&
-        matrix[3][3] > matrix[3][1] + matrix[3][2] + matrix[3][0] + matrix[3][4] &&
-        matrix[4][4] > matrix[4][1] + matrix[4][2] + matrix[4][3] + matrix[4][0])
-        return 1;
-    return 0;
-
-}
+//bool ok(int n, int m, float** matrix)
+//{
+//    if (matrix[0][0] > matrix[0][1] + matrix[0][2] + matrix[0][3] + matrix[0][4] &&
+//        matrix[1][1] > matrix[1][0] + matrix[1][2] + matrix[1][3] + matrix[1][4] &&
+//        matrix[2][2] > matrix[2][1] + matrix[2][0] + matrix[2][3] + matrix[2][4] &&
+//        matrix[3][3] > matrix[3][1] + matrix[3][2] + matrix[3][0] + matrix[3][4] &&
+//        matrix[4][4] > matrix[4][1] + matrix[4][2] + matrix[4][3] + matrix[4][0])
+//        return 1;
+//    return 0;
+//
+//}
 
 
 
@@ -88,50 +88,63 @@ int main()
 
     print(n, m, matrix);
 
-    if (!ok(n, m, matrix))
-        cout << endl << "ERRRROR" << endl << endl;
-    int x1 = 0, x2 = 0, x3 = 0, x4 = 0, x5 = 0;
-    x1 = (matrix[5][0] - matrix[0][1] * x2 - matrix[0][2] * x3 - matrix[0][3] * x4 - matrix[0][4] * x5) / matrix[0][0];
+    int* x = new int[n];
+    for (int i = 0; i < n; i++)
+    {
+        x[i] = 0;
+    }
+    x[0] = (matrix[0][5] - matrix[0][1] * x[1] - matrix[0][2] * x[2] - matrix[0][3] * x[3] - matrix[0][4] * x[4]) / matrix[0][0];
+    x[1] = (matrix[1][5] - matrix[1][0] * x[0] - matrix[1][2] * x[2] - matrix[1][3] * x[3] - matrix[1][4] * x[4]) / matrix[1][1];
+    x[2] = (matrix[2][5] - matrix[2][0] * x[0] - matrix[2][1] * x[1] - matrix[2][3] * x[3] - matrix[2][4] * x[4]) / matrix[2][1];
+    x[3] = (matrix[3][5] - matrix[3][0] * x[0] - matrix[3][2] * x[2] - matrix[3][1] * x[1] - matrix[3][4] * x[4]) / matrix[3][1];
+    x[4] = (matrix[4][5] - matrix[4][0] * x[0] - matrix[4][2] * x[2] - matrix[4][3] * x[3] - matrix[4][1] * x[1]) / matrix[4][1];
 
 
-    /* float  tmp;
-     int k;
-     float* xx = new float[m];
+     //float  tmp;
+     //int k;
+     //float* xx = new float[m];
+     //for (i = 0; i < n; i++)
+     //{
+     //    tmp = matrix[i][i];
+     //    for (j = n; j >= i; j--)
+     //        matrix[i][j] /= tmp;
+     //    for (j = i + 1; j < n; j++)
+     //    {
+     //        tmp = matrix[j][i];
+     //        for (k = n; k >= i; k--)
+     //            matrix[j][k] -= tmp * matrix[i][k];
+     //    }
+     //    print(n, m, matrix);
+     //}
+     //xx[n - 1] = matrix[n - 1][n];
+     //for (i = n - 2; i >= 0; i--)
+     //{
+     //    xx[i] = matrix[i][n];
+     //    for (j = i + 1; j < n; j++) xx[i] -= matrix[i][j] * xx[j];
+     //}
+     //for (i = 0; i < n; i++)
+     //    cout << xx[i] << " ";
+     //cout << endl;
 
-     for (i = 0; i < n; i++)
-     {
-         tmp = matrix[i][i];
-         for (j = n; j >= i; j--)
-             matrix[i][j] /= tmp;
-         for (j = i + 1; j < n; j++)
-         {
-             tmp = matrix[j][i];
-             for (k = n; k >= i; k--)
-                 matrix[j][k] -= tmp * matrix[i][k];
-         }
-         print(n, m, matrix);
-     }
-
-     xx[n - 1] = matrix[n - 1][n];
-     for (i = n - 2; i >= 0; i--)
-     {
-         xx[i] = matrix[i][n];
-         for (j = i + 1; j < n; j++) xx[i] -= matrix[i][j] * xx[j];
-     }
-
-     for (i = 0; i < n; i++)
-         cout << xx[i] << " ";
-     cout << endl;
-
-     float* r = new float[n];
+     /*float* r = new float[n];
      for (i = 0; i < n; i++) {
          r[i] = copy[i][4] - (copy[i][0] * xx[0] + copy[i][1] * xx[1] + copy[i][2] * xx[2] + copy[i][3] * xx[3]);
-     }*/
+     }
 
-     /* cout << "Error:" << endl;
+     cout << "Error:" << endl;
       for (i = 0; i < n; i++)
           cout << r[i] << " ";
       cout << endl;*/
+
+
+
+    cout << "result: " << endl;
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << x[i] << endl;
+    }
+
 
     delete[] matrix;
     return 0;
